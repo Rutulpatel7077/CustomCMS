d386ba4ba404060eed024f131dc947ef01f1fc9b
Rutulpatel7077 <rutulpatel7077@gmail.com>
2017-04-04 17:10:11 -0400

    Successfull with purechat and testing with Contact page

--- Views/content/about.php
+++ Views/content/about.php
@@ -11,7 +11,7 @@
     <div class="jumbotron">
     <h1>About Us</h1>
     <p>This is a Custom Content Management System in PHP using MVC Architecture.<br>
-        You can register with CMS and make your own website and improve all the functionality easily. So, Make your free websites<br>
+        You can register with CMS and make your own website and improve all the functionality easily. So, Make your free websites
         and create new Web world for your business or make your porfolio.
     </p>
     </div>
