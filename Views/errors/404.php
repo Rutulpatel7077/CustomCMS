966ee9016eef84a4d9cd1cc4c83ee6e434366320
Rutulpatel7077 <rutulpatel7077@gmail.com>
2017-04-04 17:51:51 -0400

    Updated 404

--- Views/errors/404.php
+++ Views/errors/404.php
@@ -8,18 +8,29 @@
 ?>
 
 
-
+<style>  .center {
+        text-align: center;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: auto;
+        margin-top: auto;
+    }</style>
 <div class="container">
     <div class="jumbotron">
-<!--        <h1>Page Not Found-->
-<!--            <small><font face="Tahoma" color="red">Error 404</font></small>-->
-<!--        </h1>-->
-<!--        <br/>-->
-<!--        <p>The page you requested could not be found, either contact your webmaster or try again. Use your-->
-<!--            browsers <b>Back</b> button to navigate to the page you have prevously come from</p>-->
-<!--        <p><b>Or you could just press this neat little button:</b></p>-->
-<!--        <a href="../../index.php" class="btn btn-large btn-info"><i class="icon-home icon-white"></i> Take-->
-<!--            Me Home</a>-->
+        <div class="row">
+            <div class="span12">
+                <div class="hero-unit center">
+                    <h1>Page Not Found
+                        <small><font face="Tahoma" color="red">Error 404</font></small>
+                    </h1>
+                    <br/>
+                    <p>The page you requested could not be found, either contact your webmaster or try again. Use your
+                        browsers <b>Back</b> button to navigate to the page you have prevously come from</p>
+                    <p><b>Or you could just press this neat little button:</b></p>
+                    <a href="../../index.php" class="btn btn-large btn-info"><i class="icon-home icon-white"></i> Take
+                        Me Home</a>
+                </div>
+            </div>
+        </div>
     </div>
-
 </div>
