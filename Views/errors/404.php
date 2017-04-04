071f845763a111a748048fe156893b68faf2fe32
Rutulpatel7077 <rutulpatel7077@gmail.com>
2017-04-04 17:39:28 -0400

    Updated 404

--- Views/errors/404.php
+++ Views/errors/404.php
@@ -11,6 +11,7 @@
 
 <style>  .center {text-align: center; margin-left: auto; margin-right: auto; margin-bottom: auto; margin-top: auto;}</style>
 <div class="container">
+    <div class="jumbotron">
     <div class="row">
         <div class="span12">
             <div class="hero-unit center">
@@ -21,5 +22,6 @@
                 <a href="../../index.php" class="btn btn-large btn-info"><i class="icon-home icon-white"></i> Take Me Home</a>
             </div>
         </div>
+    </div>
     </div>
 </div>
