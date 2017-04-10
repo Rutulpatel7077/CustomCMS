6099949757df1823fa4db397eec7e52ffbcc0b5b
Rutulpatel7077 <rutulpatel7077@gmail.com>
2017-04-10 19:12:18 -0400

    Testing

---
+++ Controllers/email.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rutul
+ * Date: 2017-04-10
+ * Time: 5:50 PM
+ */
+function ProcessEmail($fullName, $emailAddress, $contactNumber, $message)
+{
+	// If you are using Composer (recommended)
+	require 'vendor/autoload.php';
+
+// If you are not using Composer
+// require("path/to/sendgrid-php/sendgrid-php.php");
+
+	$from = new SendGrid\Email(null, $emailAddress);
+	$subject = "Email from Contact Form";
+	$to = new SendGrid\Email(null, "rutulpatel7077@gmail.com");
+
+	$message .= "\n \nFull Name:" . $fullName . "\nContact Number: " . $contactNumber;
+
+	$content = new SendGrid\Content("text/plain", $message);
+	$mail = new SendGrid\Mail($from, $subject, $to, $content);
+	$sg = new \SendGrid("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");
+	// send the mail
+	$response = $sg->client->mail()->send()->post($mail);
+
+
+	return $response->statusCode();
+}
+
+
+?>
--- Views/content/contact.php
+++ Views/content/contact.php
@@ -1,21 +1,57 @@
 <?php
-/**
- * Page Name:contact.php
- * Author: Rutul Patel
- * Student Number: 200335158
- * Description of Page: This Page is contact page for WebMaster
- */
+$messages = "";
+
+if(isset($_POST["fullname"])) {
+	$fullName = $_POST["fullname"];
+	$emailAddress = $_POST["email"];
+	$contactNumber = $_POST["contactnumber"];
+	$message = $_POST["message"];
+
+	include_once("Controllers/email.php");
+
+	if(ProcessEmail($fullName, $emailAddress, $contactNumber, $message) == 202) {
+		$messages = "Message has been sent";
+	}
+	else {
+		$messages = "Problem sending message";
+	}
+}
+
 ?>
 
+
 <div class="container">
+	<?php if ($messages != "") : ?>
+        <div class="alert alert-warning"><?php echo $messages ?></div>
+	<?php endif ?>
+    <div class="row">
+        <div class="col-md-offset-3 col-md-6">
+            <h1>Contact Us</h1>
 
-    <h1>Contact Us</h1>
-    <p>
-        Author of Website: Rutul Patel<br>
-        Purpose of Website: Making a Content Management Website<br>
-        Student Number: 200335158
-    </p>
-    <p>You can contact us on 200335158@student.georgianc.on.ca</p>
+            <form method="post" action="index.php?pageId=Contact">
+                <div class="form-group">
+                    <label for="FullNameTextBox">Full Name:</label>
+                    <input type="text" class="form-control" id="FullNameTextBox" name="fullname" placeholder="Enter Full Name" required>
+                </div>
+                <div class="form-group">
+                    <label for="EmailTextBox">Email:</label>
+                    <input type="email" class="form-control" id="EmailTextBox" name="email" placeholder="Enter Your Email" required>
+                </div>
+                <div class="form-group">
+                    <label for="ContactNumberTextBox">Contact Number:</label>
+                    <input type="tel" class="form-control" id="ContactNumberTextBox" name="contactnumber" placeholder="Enter Your Contact Number" required>
+                </div>
+                <div class="form-group">
+                    <label for="MessageTextArea">Message:</label>
+                    <textarea class="form-control" rows="5" id="MessageTextArea" name="message" placeholder="Enter Your Message" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <a href="index.php?pageId=Contact">
+                    <input type="button" class="btn btn-warning" value="Cancel"/>
+                </a>
+            </form>
 
-	<?php include_once('Scripts/contact.php'); ?>
+
+        </div>
+    </div>
 </div>
