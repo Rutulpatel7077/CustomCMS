9508a8c2bd89b5f2cf4e50828162641d143b8699
Rutulpatel7077 <rutulpatel7077@gmail.com>
2017-04-11 10:13:48 -0400

    Testing SendGrid with Azure

--- Config/database.php
+++ Config/database.php
@@ -17,9 +17,9 @@ function DBConnection()
 	try {
 
 		//local db access
-		$dsn = 'mysql:host=sql3.freemysqlhosting.net;port= 3306;dbname=sql3167430';
-		$Username = 'sql3167430';
-		$Password = 'QN1m2QWyip';
+		$dsn = 'mysql:host=sql.computerstudi.es;port= 3306;dbname=gc200335158';
+		$Username = 'gc200335158';
+		$Password = 'TxL9qDbL';
 		// instantiates a new pdo - an db object
 		return new PDO($dsn, $Username, $Password);
 	} catch (PDOException $e) {
