0c436cfb22eeac0097979b06ad27c7eb4dfe2934
Rutulpatel7077 <rutulpatel7077@gmail.com>
2017-04-11 01:18:02 -0400

    Testing SendGrid with Azure

--- Controllers/email.php
+++ Controllers/email.php
@@ -24,14 +24,12 @@ function ProcessEmail($fullName, $emailAddress, $contactNumber, $message)
 
 
 
-	$apiKey = $_ENV('SENDGRID_API_KEY');
-//	$sg = new \SendGrid($apiKey);
+//	$apiKey = getenv('SENDGRID_API_KEY');
+	//$sg = new \SendGrid($apiKey);
 
     $sg = new \SendGrid("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");
 	// send the mail
 	$response = $sg->client->mail()->send()->post($mail);
-
-
 
 	return $response->statusCode();
 }
