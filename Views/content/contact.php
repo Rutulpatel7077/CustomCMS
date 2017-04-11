e76d580920516df7f8bf743b2b767be15547714d
Rutulpatel7077 <rutulpatel7077@gmail.com>
2017-04-11 17:58:06 -0400

    Testing SendGrid with Azure

--- Views/content/contact.php
+++ Views/content/contact.php
@@ -1,7 +1,7 @@
 <?php
 $messages = "";
 
-if(isset($_POST["fullname"])) {
+if (isset($_POST["fullname"])) {
 	$fullName = $_POST["fullname"];
 	$emailAddress = $_POST["email"];
 	$contactNumber = $_POST["contactnumber"];
@@ -9,49 +9,78 @@ if(isset($_POST["fullname"])) {
 
 	include_once("Controllers/email.php");
 
-	if(ProcessEmail($fullName, $emailAddress, $contactNumber, $message) == 202) {
+	if (ProcessEmail($fullName, $emailAddress, $contactNumber, $message) == 202) {
 		$messages = "Message has been sent";
-	}
-	else {
+	} else {
 		$messages = "Problem sending message";
 	}
 }
-
 ?>
-
 
 <div class="container">
 	<?php if ($messages != "") : ?>
         <div class="alert alert-warning"><?php echo $messages ?></div>
 	<?php endif ?>
-    <div class="row">
-        <div class="col-md-offset-3 col-md-6">
-            <h1>Contact Us</h1>
 
+    <div class="jumbotron jumbotron-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-lg-12">
+                    <h1 class="h1">
+                        Contact us <small>Feel free to contact us</small></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="well well-sm">
             <form method="post" action="index.php?pageId=Contact">
                 <div class="form-group">
                     <label for="FullNameTextBox">Full Name:</label>
-                    <input type="text" class="form-control" id="FullNameTextBox" name="fullname" placeholder="Enter Full Name" required>
+                    <input type="text" class="form-control" id="FullNameTextBox" name="fullname"
+                           placeholder="Enter Full Name" required>
                 </div>
                 <div class="form-group">
                     <label for="EmailTextBox">Email:</label>
-                    <input type="email" class="form-control" id="EmailTextBox" name="email" placeholder="Enter Your Email" required>
+                    <input type="email" class="form-control" id="EmailTextBox" name="email"
+                           placeholder="Enter Your Email" required>
                 </div>
                 <div class="form-group">
                     <label for="ContactNumberTextBox">Contact Number:</label>
-                    <input type="tel" class="form-control" id="ContactNumberTextBox" name="contactnumber" placeholder="Enter Your Contact Number" required>
+                    <input type="tel" class="form-control" id="ContactNumberTextBox" name="contactnumber"
+                           placeholder="Enter Your Contact Number" required>
                 </div>
                 <div class="form-group">
                     <label for="MessageTextArea">Message:</label>
-                    <textarea class="form-control" rows="5" id="MessageTextArea" name="message" placeholder="Enter Your Message" required></textarea>
+                    <textarea class="form-control" rows="5" id="MessageTextArea" name="message"
+                              placeholder="Enter Your Message" required></textarea>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
                 <a href="index.php?pageId=Contact">
                     <input type="button" class="btn btn-warning" value="Cancel"/>
                 </a>
             </form>
+            </div>
+        </div>
 
-
+        <div class="col-md-4">
+            <form>
+                <legend><span class="glyphicon glyphicon-globe"></span> Our office</legend>
+                <address>
+                    <strong>Guuju Boy</strong><br>
+                    2 Kozlov St, 402 Apartment<br>
+                    Barrie, ON  L4N5A1<br>
+                    <abbr title="Phone">
+                        P:</abbr>
+                    (647) 470-7728
+                </address>
+                <address>
+                    <strong>Rutul Patel</strong><br>
+                    <a href="mailto:#">rutulpatel7077@gmail.com</a>
+                </address>
+            </form>
         </div>
     </div>
 </div>
