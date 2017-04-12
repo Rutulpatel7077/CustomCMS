693a48e490169d808e496bc9e30a3b0ade618a85
Rutulpatel7077 <rutulpatel7077@gmail.com>
2017-04-11 20:37:22 -0400

    Testing SendGrid with Azure

--- Views/dashboard.php
+++ Views/dashboard.php
@@ -6,17 +6,32 @@
  * Description of Page: This is dashboard for admin user.
  */
 ?>
-    <div class="container">
-        <h1>Dashboard</h1>
-        <div class="row">
-            <div class="col-md-4">
-                <ul class="dashboard-menu">
-                    <li><a href="index.php">Dashboard</a></li>
-					<?php if (isset($_SESSION["is_logged_in"])) : ?>
-                        <li><a href="index.php?pageId=PagesList">Pages List</a></li>
-					<?php endif ?>
-                </ul>
-            </div>
+<div class="container">
+    <h1>Dashboard</h1>
+    <div class="row">
+        <div class="col-md-4">
+            <ul class="dashboard-menu">
+                <li><a href="index.php">Dashboard</a></li>
+				<?php if (isset($_SESSION["is_logged_in"])) : ?>
+                    <li><a href="index.php?pageId=PagesList">Pages List</a></li>
+				<?php endif ?>
+                <div class="container">
+                    <div class="row">
+                    </div>
+
+            </ul>
         </div>
     </div>
+</div>
 <?php include_once('Scripts/pureChat.php'); ?>
+
+<?php foreach ($pages as $page) : ?>
+    <a href="index.php?pageId=PageView&pageID=
+    <div class="
+       container" style="width: 30%; display: inline-block; background-color:darkseagreen; margin: 10px; padding: 3px; text-align: center;">
+    <h1><?php echo $page['title'] ?></h1>
+    </div>
+    </a>
+
+<?php endforeach; ?>
+
--- Views/partials/footer.php
+++ Views/partials/footer.php
@@ -14,7 +14,7 @@
 </body>
 <footer>
     <div class="row">
-        <div class="col-lg-12">
+        <div class="col-lg-12 -fixed-bottom">
             <p>Copyright &copy; Rutul Patel 2017</p>
         </div>
     </div>
